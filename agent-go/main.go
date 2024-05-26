a8db2651b9283609efa80801e1931e59bcd7fc4f
Muhammad Salif <muhammad.salif@yahoo.com>
2024-05-26 07:08:11 +0500

    [Update] port moved to env

--- agent-go/main.go
+++ agent-go/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"agent-go/server/app"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Println("Error loading .env file, using default settings")
+	}
+
+	// Use the environment variable PORT or default to "8000"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
 	router := app.SetupRouter()
 
-	log.Println("Server listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Printf("Server listening on :%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
