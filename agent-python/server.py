2191d6263f0e8b123c7ecdaa2cc0bc11821296f4
Muhammad Sami <56019715+sami-lab@users.noreply.github.com>
2024-05-26 20:08:37 +0500

    agent-python

--- agent-python/server.py
+++ agent-python/server.py
@@ -1,5 +1,7 @@
 import os
-from uvicorn import run
+import ngrok
+
+from uvicorn import Config, Server, run
 from fastapi import FastAPI
 # Load environment variables
 from dotenv import load_dotenv
@@ -14,5 +16,8 @@ from app import app
 # Start the server
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8001))
-    run(app,host="0.0.0.0", port=port)
-   
+    print("-------------------------------------------",port)
+    listener =  ngrok.forward(authtoken_from_env=True, proto="http", addr="localhost:8001", domain="sami.tunnels.ctindel-ngrok.com")
+    run(app, host="0.0.0.0", port=port)
+
+
